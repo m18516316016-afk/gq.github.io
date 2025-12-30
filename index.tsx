cf28efd0b5112755f9e93f3452cf996cc677708f
m18516316016-afk <m18516316016@163.com>
2025-12-30 20:56:09 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react.tsx';
+import ReactDOM from 'react-dom/client.tsx';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
